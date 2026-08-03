1dca9a84819c473927a6a5a9400a1f8218b318cf
RoKu-Brand <tsubasa00928@gmail.com>
2026-08-03 16:35:29 +0900

    Add Edge Function HTTP helpers

---
+++ supabase/functions/_shared/http.ts
@@ -0,0 +1,16 @@
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, stripe-signature',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+};
+
+export function json(data: unknown, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { ...corsHeaders, 'Content-Type': 'application/json; charset=utf-8' },
+  });
+}
+
+export function optionsResponse() {
+  return new Response('ok', { headers: corsHeaders });
+}
